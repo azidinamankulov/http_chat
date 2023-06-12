9dda74f29794031bcd79aa5e809f85ff1a1f90d3
Azidin <azidinkun@icloud.com>
2023-06-12 21:30:13 +0600

    I have done a work

--- src/Server.java
+++ src/Server.java
@@ -10,6 +10,7 @@ public class Server {
     public void handle(Socket socket) throws IOException {
         String userName = generateRandomUser();
 
+
         System.out.printf("Connected new user: %s%n", userName);
         try (socket;
              var reader = getReader(socket);
@@ -17,65 +18,74 @@ public class Server {
             user.put(socket, userName);
             sendResponse("Hi it's your nickname: " + userName, writer);
             while (true) {
-                var message = reader.nextLine().strip();
+               String  message = reader.nextLine().strip();
                 for (Socket s :user.keySet()){
                     if (s != socket){
                         sendResponse(user.get(socket) + ": " + message, getWriter(s));
                     }
-                }
-                if (isEmptyMsg(message) || isQuitMsg(message)){
+                } if (isEmptyMsg(message)){
                     sendResponse("you can't write the empty message!!!", getWriter(socket));
+                } else if (isQuitMsg(message)){
+                    System.out.printf("the user %s",user.get(socket) + " left\n");
+                    leftOfUser(socket);
+                    break;
+
                 }
             }
         } catch (NoSuchElementException e) {
-            for (Socket s : user.keySet()) {
-                if (s != socket) {
-                    sendResponse("the user  " + user.get(socket) + "  left to bye a some milk", getWriter(s));
-                }
-            }
-            System.out.printf("the user %s",user.get(socket) + " left");
-            user.remove(socket);
+            System.out.printf("the user %s",user.get(socket) + " left\n");
+            leftOfUser(socket);
         } catch (IOException ioException) {
             throw new RuntimeException();
         }
     }
 
 
-    private static PrintWriter getWriter(Socket socket) throws IOException {
+    private PrintWriter getWriter(Socket socket) throws IOException {
         OutputStream outputStream = socket.getOutputStream();
         return new PrintWriter(outputStream);
     }
 
-    private static Scanner getReader(Socket socket) throws IOException{
+    private  Scanner getReader(Socket socket) throws IOException{
         InputStream inputStream = socket.getInputStream();
         InputStreamReader inputStreamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
         return new Scanner(inputStreamReader);
     }
 
-    private static boolean isQuitMsg(String msg){
-        return "bye".equalsIgnoreCase(msg);
-
+    private boolean isQuitMsg(String message){
+        return "bye".equals(message);
     }
 
-    public static boolean isEmptyMsg(String msg){
-        return msg == null || msg.isBlank();
+    private  boolean isEmptyMsg(String message){
+        return message == null || message.isBlank();
     }
 
-    private static void sendResponse(String response,Writer writer) throws IOException{
+    private void sendResponse(String response,Writer writer) throws IOException{
         writer.write(response);
         writer.write(System.lineSeparator());
         writer.flush();
     }
 
-    static String generateRandomUser() {
+    private String generateRandomUser() {
         String alphabet = "abcdefghijklmnopqrstuvwxyz";
         StringBuilder user = new StringBuilder();
         Random random = new Random();
         int userNameLength = random.nextInt(3) + 4;
         for (int i = 0; i < userNameLength; i++) {
             user.append(alphabet.charAt(random.nextInt(26)));
+        } return user.toString().toUpperCase().strip();
+    }
+
+    private void leftOfUser(Socket socket){
+        try {
+            for (Socket s : user.keySet()) {
+                if (s != socket ) {
+                    sendResponse("the user  " + user.get(socket) + "  left", getWriter(s));
+                }
+            } user.remove(socket);
+        }catch (IOException e){
+            throw new RuntimeException();
         }
-        return user.toString().toUpperCase().strip();
     }
 
 }
